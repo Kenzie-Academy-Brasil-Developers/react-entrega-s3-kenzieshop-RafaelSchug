4c01287cea2e6df25d8eb9bd1fcffa35099178e5
Rafael Schug <rafaelschugbc@gmail.com>
2021-10-01 11:00:19 -0300

    add: Kenzie logo and container animations change: css settings for mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import { useHistory } from "react-router";
 import { useSelector } from "react-redux";
 import {GrCart, GrList} from 'react-icons/gr';
 import './style.css';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
 
@@ -11,6 +12,10 @@ const Header = () => {
 
     return (
         <header>
+            <div className='logo'>
+                <img src={logo} alt="Kenzie logo" />
+                <h4>Kenzie Shop</h4>
+            </div>
             <nav>
                 <ul>
                     <li onClick={() => history.push('/cart')}><GrCart/><span>{cart.reduce((acc, item)=> acc + item.quantity, 0)}</span>Carrinho</li>
